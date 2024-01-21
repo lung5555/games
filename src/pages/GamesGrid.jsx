040928931f5c2984c2cae7e7e46032706492b040
lung5555 <156530119+lung5555@users.noreply.github.com>
2024-01-22 02:26:02 +0800

    enhance grid

--- src/pages/GamesGrid.jsx
+++ src/pages/GamesGrid.jsx
@@ -12,6 +12,7 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import CircularProgress from '@mui/material/CircularProgress';
+import NativeSelect from '@mui/material/NativeSelect';
 
 const GamesGrid = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -24,10 +25,14 @@ const GamesGrid = () => {
         fetchData(searchQuery, page, sortBy);
     }, []);
 
-    const fetchData = async (searchQuery, pageNo, sortBy) => {
+    const fetchData = async (searchQuery, pageNo, sortBy, resetData = true) => {
         try {
-            const response = await axios.get(`/api/games?pageNo=${pageNo + 1}&pageSize=100` + (sortBy ? ('&sortBy=' + sortBy) : '') + (searchQuery ? ('&q=' + searchQuery) : ''));
-            setData(response.data);
+            const response = await axios.get(`/api/games?pageNo=${pageNo + 1}&pageSize=50` + (sortBy ? ('&sortBy=' + sortBy) : '') + (searchQuery ? ('&q=' + searchQuery) : ''));
+            if (resetData) {
+                setData(response.data);
+            } else {
+                setData(data.concat(response.data));
+            }
             setIsLoading(false);
         } catch (error) {
             console.error('Failed to fetch data:', error);
@@ -35,111 +40,130 @@ const GamesGrid = () => {
     };
 
     const handleScroll = () => {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            setPage((prevPage) => prevPage + 1);
+        if (!isLoading && (window.innerHeight + window.scrollY >= document.body.offsetHeight)) {
+            console.log("handleScroll: ", page)
+            setPage(page + 1);
+            setIsLoading(true)
+            fetchData(searchQuery, page + 1, sortBy);
         }
     };
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    function getDisplayPrice(currentPrice, regularPrice) {
-        if (currentPrice >= regularPrice) {
-            return (<Grid item>
-                <Typography variant="h5" align="right" component="div">
-                    ${currentPrice}
-                </Typography>
-            </Grid>);
-        }
-
-        return (<Grid item sx={{ alignSelf: 'flex-end' }}>
-            <Typography variant="h5" align="right" component="div">
-                <span style={{ color: '#ed934e' }}><b>${currentPrice}</b></span>
-            </Typography>
-            <Typography variant="body1" align="right" component="div">
-                <span style={{ textDecoration: 'line-through' }}>${regularPrice}</span>
-            </Typography>
-        </Grid>);
-    }
-
-    function getDiscountLabel(regularPrice, discountRate, startAt, endAt) {
-        if (!discountRate) {
-            return;
-        }
-
-        const startAtLabel = startAt ? new Date(Date.parse(startAt)).toLocaleDateString('zh-HK') : '-';
-        const endAtLabel = endAt ? new Date(Date.parse(endAt)).toLocaleDateString('zh-HK') : '-';
-
-        return (<Grid container justifyContent="space-between">
-            <Grid item xs={8}>
-                <Typography variant="body2" sx={{ padding: "0 5px" }} >
-                    優惠期: {startAtLabel} ~ {endAtLabel}
-                </Typography>
-            </Grid>
-            <Grid item xs={4}>
-                <Typography variant="h6" align="right" >
-                    <span style={{ color: '#eb5252' }}>(↓{discountRate}%) </span>
-                    <span style={{ textDecoration: 'line-through' }}>${regularPrice}</span>
-                </Typography>
-                <Typography variant="body1" align="right" >
-
-                </Typography>
-            </Grid>
-        </Grid>);
-    }
-
     const handleSearch = () => {
-        setPage(1);
+        setPage(0);
         setData([]);
+        setIsLoading(true)
         fetchData();
     };
 
     const handleSortChange = (event) => {
-        // setSortOption(event.target.value);
+        console.log("handleSortChange");
+        setSortBy(event.target.value);
+        setPage(0);
+        setData([]);
+        setIsLoading(true)
+        fetchData(searchQuery, 0, event.target.value);
     };
+
+    useEffect(() => {
+        // window.addEventListener('scroll', handleScroll);
+        // return () => {
+        //     window.removeEventListener('scroll', handleScroll);
+        // };
+    }, []);
+
+    function getCurrentPriceLabel(data) {
+        return <Grid container justifyContent="space-between" alignItems="flex-end">
+            <Grid item>
+                <Typography variant="h5" sx={{ padding: "0 0 0 5px" }} display="inline">
+                    <span style={{ color: '#ed934e' }}><b>${data.currentPrice}</b></span>
+                </Typography>
+            </Grid>
+            <Grid item>
+                {getCheapestPriceLabel(data)}
+            </Grid>
+        </Grid>
+    }
+
+    function getCheapestPriceLabel(data) {
+        if (!data.cheapestPriceEndAt || !data.cheapestPrice) {
+            return;
+        }
+
+        const dateLabel = data.cheapestPriceEndAt ? new Date(Date.parse(data.cheapestPriceEndAt)).toLocaleDateString('zh-HK') : '-';
+        return (<Typography variant="caption" sx={{ padding: "0 5px 0 0" }} display="inline">
+            <span style={{ color: '#8f8f8f' }}>${data.cheapestPrice} ({dateLabel})</span>
+        </Typography>);
+    }
+
+    function getPriceLabel(data) {
+        if (!data.discountRate) {
+            return <Grid container alignItems="flex-end">
+                <Grid item xs={12} sm={12} md={12} lg={12} >
+                    {getCurrentPriceLabel(data)}
+                </Grid>
+            </Grid>;
+        }
+
+        const startAtLabel = data.discountStartAt ? new Date(Date.parse(data.discountStartAt)).toLocaleDateString('zh-HK') : '-';
+        const endAtLabel = data.discountEndAt ? new Date(Date.parse(data.discountEndAt)).toLocaleDateString('zh-HK') : '-';
+
+        return (<Grid container alignItems="flex-end">
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Typography variant="subtitle2" sx={{ padding: "0 5px" }} >
+                    <span style={{ color: '#999999' }}>{startAtLabel} ~ {endAtLabel} </span>
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+                <Typography variant="body1" sx={{ padding: "0 0 0 5px" }} >
+                    <span style={{ color: '#c4c4c4', textDecoration: 'line-through' }}><b>${data.regularPrice}</b></span>
+                    <span style={{ color: '#eb5252' }}> (↓{data.discountRate}%)</span>
+                </Typography>
+                {getCurrentPriceLabel(data)}
+            </Grid>
+        </Grid>);
+    }
 
     return (
         <div>
             <AppBar position="sticky">
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <SearchIcon />
-                    </IconButton>
-                    <TextField
-                        label="Search"
-                        variant="outlined"
-                    // value={searchTerm}
-                    // onChange={(e) => setSearchTerm(e.target.value)}
-                    />
+                    <Grid container justifyContent="space-between" alignItems="stretch">
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <SearchIcon />
+                        </IconButton>
+                        <TextField
+                            label="Search"
+                            variant="outlined"
+                        // value={searchTerm}
+                        // onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <NativeSelect value={sortBy} onChange={handleSortChange}>
+                            <option value="-discountStartAt">最新優惠</option>
+                            <option value="discountEndAt">就完優惠</option>
+                            <option value="currentPrice">售價</option>
+                            <option value="discountRate">折扣</option>
+                        </NativeSelect>
+                    </Grid>
                 </Toolbar>
             </AppBar>
             <Container sx={{ padding: "0 0" }} >
-                <Grid container justifyContent="flex-start" alignItems="center" spacing={1} >
+                <Grid container justifyContent="center" alignItems="center" spacing={0.5} >
                     {isLoading && <Grid item justifyContent="center" alignItems="center"><CircularProgress sx={{ padding: "10px 0" }} /></Grid>}
                     {data.map((item) => (
-                        <Grid item xs={12} sm={12} md={12} lg={12} key={item.id}>
+                        <Grid item xs={12} sm={6} md={6} lg={6} key={item.id}>
                             <Card>
-                                <Grid container>
-                                    <Grid item xs={12} sm={6} md={6} lg={6}>
+                                <Grid container >
+                                    <Grid item xs={6} sm={6} md={6} lg={6}>
                                         <CardActionArea href={item.link} target="_blank" referrerPolicy="no-referrer" >
                                             {/* style={{ width: (window.innerWidth > 600 ? 600 : window.innerWidth), height: 'auto' }} */}
                                             <CardMedia component="img" sx={{ objectFit: "contain" }} image={item.image} alt={item.name} referrerPolicy="no-referrer" />
                                         </CardActionArea>
                                     </Grid>
-                                    <Grid container sm={6} md={6} lg={6}>
-                                        <Grid item xs={10} sm={12} md={12} lg={12}>
-                                            <Typography variant="body1" sx={{ padding: "0 5px" }}>{item.name}</Typography>
+                                    <Grid container item xs={6} sm={6} md={6} lg={6}>
+                                        <Grid item xs={12} sm={12} md={12} lg={12}>
+                                            <Typography variant="body2" sx={{ padding: "0 5px", display: "-webkit-box", overflow: "hidden", textOverflow: "ellipsis", WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}>{item.name}</Typography>
                                         </Grid>
-                                        <Grid item xs={2} sm={12} md={12} lg={12} >
-                                            <Typography variant="h5" sx={{ padding: "0 5px" }} align="right" >
-                                                <span style={{ color: '#ed934e' }}><b>${item.currentPrice}</b></span>
-                                            </Typography>
-                                        </Grid>
-                                        {getDiscountLabel(item.regularPrice, item.discountRate, item.discountStartAt, item.discountEndAt)}
+                                        {getPriceLabel(item)}
                                     </Grid>
                                 </Grid>
                             </Card>
